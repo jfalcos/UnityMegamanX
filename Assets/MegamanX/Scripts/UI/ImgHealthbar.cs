1f3b4c4a65e3d9e57b53b6d515af2f4d06837b27
Dunmord <javierfalconc@gmail.com>
2015-09-17 11:10:20 -0400

    Added a rough of megaman's UI healthbar

---
+++ Assets/MegamanX/Scripts/UI/ImgHealthbar.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+[RequireComponent (typeof(Image))]
+public class ImgHealthbar : MonoBehaviour
+{
+	public Image img = null;
+	public Hitpoints hitpoints = null;
+
+	void Awake()
+	{
+		img = GetComponent<Image> ();
+	}
+
+	void OnEnable()
+	{
+		hitpoints.onDamage += OnDamage;
+	}
+
+	void OnDisable()
+	{
+		hitpoints.onDamage -= OnDamage;
+	}
+
+	public void OnDamage(Hitpoints hitpoints)
+	{
+		img.fillAmount = hitpoints.GetRemainingHealthPercentage () / 100;
+	}
+}
